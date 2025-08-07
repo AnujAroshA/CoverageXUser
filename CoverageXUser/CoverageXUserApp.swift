b3a2175d318c51ebdf36050cc9c07bc7cbcef6e7
AnujAroshA <arosha.ict@gmail.com>
2025-08-07 16:06:22 +0530

    Initial Commit

---
+++ CoverageXUser/CoverageXUserApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoverageXUserApp.swift
+//  CoverageXUser
+//
+//  Created by Arosha Piyadigama on 2025-08-07.
+//
+
+import SwiftUI
+
+@main
+struct CoverageXUserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoverageXUserTests/CoverageXUserTests.swift
@@ -0,0 +1,17 @@
+//
+//  CoverageXUserTests.swift
+//  CoverageXUserTests
+//
+//  Created by Arosha Piyadigama on 2025-08-07.
+//
+
+import Testing
+@testable import CoverageXUser
+
+struct CoverageXUserTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
